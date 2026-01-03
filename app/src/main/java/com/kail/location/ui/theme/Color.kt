f94a7d5b23396a086e4dd0139196a737d4cb4d62
noellegazelle6 <noellegazelle6@gmail.com>
2026-01-03 18:15:22 +0800

    updata
    改包名前

--- app/src/main/java/com/kail/location/ui/theme/Color.kt
+++ app/src/main/java/com/kail/location/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.kail.location.ui.theme
+package com.zcshou.gogogo.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val PrimaryColor = Color(0xFF008577) // From XML colorPrimary
+val PrimaryDarkColor = Color(0xFF00574B) // From XML colorPrimaryDark
+val AccentColor = Color(0xFFD81B60) // From XML colorAccent
